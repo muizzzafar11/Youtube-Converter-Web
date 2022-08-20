1330eb2ba8c098e6c6fe9738b739f4e066fa3bf6
Muizz Zafar <muizzzafar25351964@gmail.com>
2022-08-20 07:05:54 -0400

    updated file

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-
+    apiKey: "AIzaSyBuwebJgQWGKUdO2Y3DI3BhYpQXxQLRpRQ",
+    authDomain: "firestore-database-a2257.firebaseapp.com",
+    projectId: "firestore-database-a2257",
+    storageBucket: "firestore-database-a2257.appspot.com",
+    messagingSenderId: "540551890671",
+    appId: "1:540551890671:web:ad794cad13c87e90b1bd1a",
+    measurementId: "G-4CD5TRD9JV"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
